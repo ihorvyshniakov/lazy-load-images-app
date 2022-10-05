d6dbb2df3f34ba3c586f8f7b42fe30267b1be217
Vyshniakov Ihor <ihor.vyshniakov@qlicks.nl>
2022-10-06 00:54:38 +0300

    4_lesson_done: finish(added loading images, when scrolled to them)

--- src/components/LoadableImage/LoadableImage.js
+++ src/components/LoadableImage/LoadableImage.js
@@ -1,31 +1,43 @@
 import React, { useEffect, useRef, useState } from 'react';
 import s from './LoadableImage.module.css';
 import cn from 'classnames';
+import useOnScreen from '../../hooks/useOnScreen';
 
 const LoadableImage = ({ src, alt = '', onLoad = () => {} }) => {
 	const [isLoaded, setIsLoaded] = useState(false);
 	const imageRef = useRef(null);
+	const containerRef = useRef(null);
+	const isVisible = useOnScreen(containerRef);
 
 	useEffect(() => {
-		if (imageRef.current) {
-			imageRef.current.onload = () => setIsLoaded(true);
+		if (!isVisible || isLoaded) {
+			return;
 		}
-	}, []);
+		if (imageRef.current) {
+			imageRef.current.onload = () => {
+				setIsLoaded(true);
+				onLoad();
+			};
+		}
+	}, [isVisible, onLoad, isLoaded]);
 
 	return (
 		<div
+			ref={containerRef}
 			className={cn(s.container, {
 				[s.containerLoaded]: isLoaded,
 			})}
 		>
-			<img
-				ref={imageRef}
-				className={cn(s.image, {
-					[s.imageLoaded]: isLoaded,
-				})}
-				src={src}
-				alt={alt}
-			/>
+			{(isVisible || isLoaded) && (
+				<img
+					ref={imageRef}
+					className={cn(s.image, {
+						[s.imageLoaded]: isLoaded,
+					})}
+					src={src}
+					alt={alt}
+				/>
+			)}
 		</div>
 	);
 };
